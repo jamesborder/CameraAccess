c8eef10cb62dbe7013eeaa288a7f2d40549cc420
jamesborder <me@jamesborder.com>
2026-02-19 09:02:03 -0600

    Initial Commit

---
+++ CameraAccess/CameraAccessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAccessApp.swift
+//  CameraAccess
+//
+//  Created by James Border on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraAccessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraAccessTests/CameraAccessTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAccessTests.swift
+//  CameraAccessTests
+//
+//  Created by James Border on 2/19/26.
+//
+
+import Testing
+@testable import CameraAccess
+
+struct CameraAccessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
